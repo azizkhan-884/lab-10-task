aa5c5511497b9f99000beea2ec8541acdca54a2f
azizkhan-884 <150135388+azizkhan-884@users.noreply.github.com>
2024-03-21 01:28:53 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<?php
+
+$name = "user";
+$value = "aziz";
+$expires = time() + 3600; // 1 hour from now
+$path = "/";
+setcookie($name, $value, $expires, $path);
+
+// Retrieving Cookies in PHP
+
+if (isset($_COOKIE['user'])) {
+    echo 'User: ' . htmlspecialchars($_COOKIE['user']);
+} else {
+    echo 'User cookie is not set.';
+}
+
+// Deleting Cookies
+
+// Set the expiration date to one hour ago
+// setcookie("user", "", time() - 3600, "/");
+
+?>
